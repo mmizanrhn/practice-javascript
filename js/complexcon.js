1b90223ff98139607a81d7e23e95479c69acdd69
mmizanrhn <mizanrhn1@gmail.com>
2018-01-20 17:27:46 +0600

    condition

---
+++ js/complexcon.js
@@ -0,0 +1,10 @@
+var balance = 5000;
+
+if(balance >= 0){
+	alert("Yes, This is positive balance");
+	if (balance > 1000) {
+		alert("This is true");
+	}
+}else{
+	alert("This is false");
+}
---
+++ js/ifcondition.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = 20;
+
+if(a < b){
+	alert("Yes, It is true");
+}
+
+if(a == b){
+	alert("Yes, It is equal");
+}
