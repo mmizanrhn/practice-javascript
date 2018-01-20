9eb411ac3f9d2daf3ebebe34f3d85ad131da9dfd
mmizanrhn <mizanrhn1@gmail.com>
2018-01-20 19:05:54 +0600

    switchCase

---
+++ js/switch.js
@@ -0,0 +1,19 @@
+var price = "primium";
+var message = prompt("What is your price grade?");
+
+switch(message){
+	case "gold":
+	alert("It's false");
+	break;
+	
+	case "platinum":
+	alert("It's false");
+	break;
+	
+	case "primium":
+	alert("It's true");
+	break;
+
+	case "default":
+	alert("It's false");
+}
