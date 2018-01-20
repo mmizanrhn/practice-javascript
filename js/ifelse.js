243cffb0c4021f81dc13992983672420c4d91633
mmizanrhn <mizanrhn1@gmail.com>
2018-01-20 17:59:20 +0600

    ifelse

---
+++ js/ifelse.js
@@ -0,0 +1,10 @@
+var price = "primium";
+var message = prompt("wright your price");
+
+if(message == "platinum"){
+	alert("It's false");
+}else if (message == "golden") {
+	alert("Yes, It's false");
+}else if (message == "primium") {
+	alert("Yes, It's true");
+}
