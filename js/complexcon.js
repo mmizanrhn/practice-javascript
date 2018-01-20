e234646b9909b9857b290b30f27a1024a2ac5d52
mmizanrhn <mizanrhn1@gmail.com>
2018-01-20 17:32:05 +0600

    modifyCondition

--- js/complexcon.js
+++ js/complexcon.js
@@ -2,9 +2,20 @@ var balance = 5000;
 
 if(balance >= 0){
 	alert("Yes, This is positive balance");
+	
 	if (balance > 1000) {
 		alert("This is true");
+
+		if (balance > 3000) {
+			alert("yes, It's true");
+
+			if (balance == 5000) {
+				alert("Yes, It's true condition");
+			}
+		}
+
 	}
+
 }else{
 	alert("This is false");
 }
