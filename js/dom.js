d6608c46a41f9f86dbe3bf1ce3a4780c64627253
mmizanrhn <mizanrhn1@gmail.com>
2018-01-21 16:57:59 +0600

    dom

---
+++ js/dom.js
@@ -0,0 +1,2 @@
+var headline = document.getElementById("heading");
+headline.innerHTML = "Wow , This is a new headline";
---
+++ js/string.js
@@ -0,0 +1,13 @@
+var name = "Mohammad Mizanur Rahman";
+document.write(name.length);
+document.write("</br>");
+document.write(name.toUpperCase() );
+document.write("</br>");
+document.write(name.toLowerCase() );
+document.write("</br>");
+
+var position = name.indexOf("Mizanur");
+document.write(position);
+document.write("</br>");
+var strSlice = name.slice(9,16);
+document.write(strSlice);
