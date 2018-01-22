0c059de8aa010d27b581261bba01e27ec7da8f2f
mmizanrhn <mizanrhn1@gmail.com>
2018-01-22 16:55:26 +0600

    add event

--- js/event.js
+++ js/event.js
@@ -1,5 +1,10 @@
 var headline = document.getElementById("heading");
+var heading = document.getElementById("head");
 
 headline.onclick = function(){
 	headline.innerHTML = "You have clicked the heading";
+};
+
+heading.onmouseover = function(){
+	heading.innerHTML = "This is new paragraph";
 };
